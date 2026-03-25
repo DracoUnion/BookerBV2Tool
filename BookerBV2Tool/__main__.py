10d5a8b354c36ede08e2bceca200bb63a9a401ee
wizardforcel <562826179@qq.com>
2026-03-25 16:57:43 +0800

    2026-03-25 16:57:43

--- BookerBV2Tool/__main__.py
+++ BookerBV2Tool/__main__.py
@@ -2,6 +2,7 @@ import argparse
 import sys
 import os
 from . import __version__
+from .slice import slice
 
 def main():
     parser = argparse.ArgumentParser(prog="BookerGptTool", formatter_class=argparse.RawDescriptionHelpFormatter)
@@ -9,14 +10,21 @@ def main():
     parser.set_defaults(func=lambda x: parser.print_help())
     subparsers = parser.add_subparsers()
     
-    '''
-    trans_parser = subparsers.add_parser("trans-yaml", help="translate YAML files")
-    trans_parser.add_argument("fname", help="yaml file name of dir")
-    trans_parser.add_argument("-p", "--prompt", default=DFT_TRANS_PROMPT, help="prompt for trans")
-    trans_parser.add_argument("-l", "--limit", type=int, default=3000, help="max token limit")
-    trans_parser.add_argument("-t", "--threads", type=int, default=8, help="thread num")
-    trans_parser.set_defaults(func=trans_yaml_handle)
-    '''
+    slice_parser = subparsers.add_parser("trans-yaml", help="translate YAML files")
+    slice_parser.add_argument('audio', type=str, help='The audio to be sliced')
+    slice_parser.add_argument('-o', '--out', type=str, help='Output directory of the sliced audio clips')
+    slice_parser.add_argument('-dt', '--db_thresh', type=float, required=False, default=-40,
+                        help='The dB threshold for silence detection')
+    slice_parser.add_argument('-ml', '--min_length', type=int, required=False, default=5000,
+                        help='The minimum milliseconds required for each sliced audio clip')
+    slice_parser.add_argument('-mi', '--min_interval', type=int, required=False, default=300,
+                        help='The minimum milliseconds for a silence part to be sliced')
+    slice_parser.add_argument('-hs', '--hop_size', type=int, required=False, default=10,
+                        help='Frame length in milliseconds')
+    slice_parser.add_argument('-ms', '--max_sil_kept', type=int, required=False, default=500,
+                        help='The maximum silence length kept around the sliced clip, presented in milliseconds')
+    slice_parser.set_defaults(func=slice)
+
 
     args = parser.parse_args()
     args.func(args)
