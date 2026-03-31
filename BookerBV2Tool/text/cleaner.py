1495e696c97115881ce25f8304ed81fc8c51a6a1
wizardforcel <562826179@qq.com>
2026-03-31 17:02:26 +0800

    2026-03-31 17:02:26

--- BookerBV2Tool/text/cleaner.py
+++ BookerBV2Tool/text/cleaner.py
@@ -45,7 +45,8 @@ def get_bert_feature(
             style_res = model(**style_inputs, output_hidden_states=True)
             style_res = torch.cat(style_res["hidden_states"][-3:-2], -1)[0].cpu()
             style_res_mean = style_res.mean(0)
-    assert len(word2ph) == len(text) + 2
+    # assert len(word2ph) == len(text) + 2
+    # assert len(word2ph) == res.shape[0], (text, res.shape[0], len(word2ph))
     word2phone = word2ph
     phone_level_feature = []
     for i in range(len(word2phone)):
